65bac0d0b506b6840a1d101cc062951f4e714eb3
Hossein Yousefi <hossein@identi.app>
2020-11-26 20:31:16 +0100

    dockerized

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import express from "express";
 
 const app = express();
 
-app.get('/start', (req, res) => {
-    const code = req.query.code;
-    res.redirect(`https://telegram.me/${process.env.TELEGRAM_BOT_NAME}?start=${code}`);
-});
-
 app.listen(process.env.PORT, () => {
     console.log('Started listening...');
 });
